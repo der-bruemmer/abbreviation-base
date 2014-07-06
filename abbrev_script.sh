76b8bbff71b3d120e2915bf4879c3ff7918b3a18
Divyum Rastogi <divyumrastogi@gmail.com>
2014-07-06 20:22:40 +0530

    server script

---
+++ abbrev_script.sh
@@ -0,0 +1,22 @@
+#1 run downloader_extractor code
+#2 create graph
+#3 import the files to virtuoso (upload all zipped files) i.e run isql-vt from command line
+#4 run the lemon maker code
+#5 delete graph
+#6 delete folder named by language in /home/akswadmin/dbpedia_files/
+#sparql select * where { <http://dbpedia.org/resource/BC> owl:sameAs ?s };
+language = "en"
+run downloader_extractor code
+python /home/akswadmin/abbrev_repo/Downloader_Extractor code/download_extract.py $language
+echo "Files for $language downoaded successfully"
+isql-vt 1111 dba dba << END
+sparql create graph <http://dbpedia.org/> ;
+ld_dir_all('/home/akswadmin/dbpedia_files/$language/data', '*.*', 'http://dbpedia.org');
+select * from DB.DBA.LOAD_LIST;
+rdf_loader_run(); 
+END
+python3 /home/akswadmin/abbrev_repo/lemon file/extractor_lemonMaker.py $language
+isql-vt 1111 dba dba << END
+sparql CLEAR GRAPH <http://dbpedia.org/> ;
+END
+rm -r /home/akswadmin/dbpedia_files/$language
