982b06b32fb5abc8b0d039cd532362c4bc150d77
Divyum Rastogi <divyumrastogi@gmail.com>
2014-07-14 21:03:06 +0530

    updated file as per server

--- lemon file/extractor_lemonMaker.py
+++ lemon file/extractor_lemonMaker.py
@@ -16,6 +16,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 import re
 global language
 
+
 def getDisambiguations(abbrev, uri):
     global language
     query = "select distinct ?o, (str(?name) AS ?label), ?name where {"+uri+" <http://dbpedia.org/ontology/wikiPageDisambiguates> ?o. ?o <http://www.w3.org/2000/01/rdf-schema#label> ?name.FILTER(langMatches(lang(?name), \""+language.upper()+"\")) }"
@@ -137,31 +138,46 @@ def main(argv):
     #in_directory = './DBpedia/'
     #out_directory = './'+language
     in_directory = '/home/akswadmin/dbpedia_files/'
-    out_directory = '/home/akswadmin/abbrev_extracted/'+language
+    out_directory = '/home/akswadmin/'+language
     if not os.path.exists(out_directory):
         os.makedirs(out_directory)
     
     infile = in_directory+language+'/data/redirects_en.ttl'  #location of input file
     outfile = out_directory+'/abbreviation_tsv_'+language+'.txt'  #output file
     lemon_file = out_directory+'/abbreviation_lemon_'+language+'.ttl'  #lemon file
+    testTtl = out_directory+"/test.ttl" #test file
+    testTsv = out_directory+"/testTsv.txt" #test file for tsv
     input_file = open(infile,'r')
     output = open(outfile,'w')
     lemon = open(lemon_file,'w')
+    TTLFile = open(testTtl,"w") #-------------------------------------TESTS-----------------------
+    TSVFile = open(testTsv,"w") #----------------------------------------TEST-------------------------------
     abbrevs = collections.OrderedDict()
     output.write("Abbreviation\tDefinition\tLabel\tReference Link\towl:sameAS\trdf:type\n")
     lemon.write("@prefix :  <http://nlp.dbpedia.org/abbrevbase> .\n@prefix lemon: <http://lemon-model.net/lemon#> .\n@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n@prefix owl: <http://www.w3.org/2002/07/owl#> .\n@prefix dcterms: <http://purl.org/dc/terms/> .\n\n")
     lemon.write('\n<http://nlp.dbpedia.org/abbrevbase/lexicon/'+language+'>\n a lemon:Lexicon ;\n\tlemon:language "'+language+'" ;\n')
     count = 0
     count_line=0
+    flag=0
     for line in input_file:
-        """if count_line == 1:
-            break
+        """if count_line<52000:
+            #print(count_line)
+            count_line+=1
+            continue
         count_line+=1"""
         uris = line.split(" ")		#splits the triple and stores is as list
         abbrev = uris[0][uris[0].rfind("resource/")+9:-1]    #stores abbreviation
-        print(uris)
+        #if count_line%1000 == 0:
+            #print(count_line,": ",uris)
+        TTLFile.write(' '.join(uris)) #write uri -------------------------------TEST--------------------------------------
+        TTLFile.close()
+        TTLFile = open(testTtl,"a")
         if abbrev.endswith("..."):
             continue
+        """if abbrev != "O.J.":
+            continue
+        if abbrev == "O.J.":
+            flag=1"""
         #meaning = uris[2][uris[2].rfind("/")+1:-1]
         #meaningURI = uris[2][1:-1]
         if "_" not in abbrev:
@@ -219,23 +235,60 @@ def main(argv):
                                 string_to_write += URI + temp_abbr.split(" ")[0] + "#sense>, "
                 string_to_write = string_to_write[:-2]+' ;\n\ta lemon:LexicalEntry .\n\n'+URI+abbrevString+'#form>\n\tlemon:writtenRep \"'+abbrevString+'\"@'+language+' ;\n\ta lemon:LexicalForm .\n\n'
                 lemon.write(string_to_write)
+                lemon.close()
+                lemon = open(lemon_file,"a")
         
-        sameAs_string = urlFormat(v[3])		#converts sameAs from list to string format
-
-        rdfType_string = urlFormat(v[4])  #converts rdfType from list to string format
-        
-        cat_string = urlFormat(v[5])  #converts category from list to string format
-        
+        try:
+                if len(v[3])>0:
+                        sameAs_string = urlFormat(v[3])
+                else:
+                        sameAs_string = ""
+        except IndexError:
+           	sameAs_string = ""	#converts sameAs from list to string forma
+        try:
+                if len(v[4])>0:
+                        rdfType_string = urlFormat(v[4])
+                else:
+                        rdfType_string = ""  #converts rdfType from list to string format
+        except IndexError:
+                rdfType_string = ""
+        try:
+                if len(v[5])>0:
+                        cat_string = urlFormat(v[5])  #converts category from list to string format
+                else:
+                        cat_string = ""
+        except IndexError:
+                cat_string = ""
         v2= "<"+v[2]+">"
         #print(sameAs_string,"\n\n\n",rdfType_string,"\n\n\n",cat_string)
         #s=input()
-        #print(abbrevString+"\t"+v[1]+"\t"+'"'+v[1]+'"@'+language+"\t"+v[2]+"\t"+sameAs_string+"\t"+rdfType_string+"\t"+cat_string+"\n")
-        output.write(abbrevString+"\t"+v[1]+"\t"+'"'+v[1]+'"@'+language+"\t"+v2+"\t"+sameAs_string+"\t"+rdfType_string+"\t"+cat_string+"\n")
+        tsv = abbrevString+"\t"+v[1]+"\t"+'"'+v[1]+'"@'+language+"\t"+v[2]+"\t"+sameAs_string+"\t"+rdfType_string+"\t"+cat_string+"\n"
+        TSVFile.write(tsv)
+        TSVFile.close()
+        TSVFile = open(testTsv,"a")
+        try:
+                #print("in try")
+                output.write(abbrevString+"\t"+v[1]+"\t"+'"'+v[1]+'"@'+language+"\t"+v2+"\t"+sameAs_string+"\t"+rdfType_string+"\t"+cat_string+"\n")
+                output.close()
+                output = open(outfile,"a")
+                #print(tsv,"\n")
+        except:
+
+                print(tsv,"\n")
+                print("v[1]: ",type(v[1]),"\nv[2]: ",type(v2),"\n",type(abbrevString),"\n",type(sameAs_string),"\n",type(rdfType_string),"\n",type(cat_string))
+              
         if k[-1]!='.' and k[-1]!='?' and k[-1]!='!':
                 k1 = k.split(" ")[1]
         elif k[-1]=='.' or k[-1]=='?' or k[-1]=='!':
                 k1 = ""
+        #v[1]=str(v[1])
+        v[1]=v[1].replace('"',"'")
         definition = URI+abbrevString+"#sense"+k1+">\n\tlemon:definition [\n\t\tlemon:value "+'"'+v[1]+'"@'+language+"\n\t] ;\n\t" #def
+        rdf_type = ""
+        label = ""
+        category = ""
+        owl_sameAs = ""
+        ref = ""
         if len(rdfType_string) > 0:
                 rdf_type = "rdf:type " + rdfType_string + " ;\n\t"		#instance types
         
@@ -243,13 +296,14 @@ def main(argv):
 
         if len(cat_string) > 0:
                 category = "dcterms:subject " + cat_string + " ;\n\t"		#Categories
-        
         if len(sameAs_string) > 0:
                 owl_sameAs = "owl:sameAs " + sameAs_string + " ;\n\t"		#interlanguage links containing "dbpedia"
 
         ref = "lemon:reference " + v2 + " ;\n\ta lemon:LexicalSense .\n\n"	#reference
         sense = definition + rdf_type + label + category + owl_sameAs + ref	
         lemon.write(sense)
+        lemon.close()
+        lemon = open(lemon_file,"a")
     output.close()
     lemon.close()
 
